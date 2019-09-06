bcf4e8a813de82c0c50eb14b0434ac91b40bc2d5
Ivan Muñoz <ivanalemunioz@gmail.com>
2019-09-06 18:26:59 -0300

    Afip.js refactorized to use async/await

--- src/Afip.js
+++ src/Afip.js
@@ -2,6 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const soap = require('soap');
 const forge = require('node-forge');
+const xml2js = require('xml2js');
+
+// XML parser
+var xmlParser = new xml2js.Parser({
+	normalizeTags: true,
+	normalize: true,
+	explicitArray: false,
+	attrkey: 'header',
+	tagNameProcessors: [key => key.replace('soapenv:', '')]
+});
+
+// Available Web Services
+//const ElectronicBilling = require('./Class/ElectronicBilling');
+//const RegisterScopeFour = require('./Class/RegisterScopeFour');
+//const RegisterScopeFive = require('./Class/RegisterScopeFive');
+//const RegisterScopeTen = require('./Class/RegisterScopeTen');
+//const RegisterScopeThirteen = require('./Class/RegisterScopeThirteen');
 
 /**
  * Software Development Kit for AFIP web services
@@ -18,8 +35,7 @@ const forge = require('node-forge');
  **/
 module.exports = Afip;
 
-function Afip(options){
-
+function Afip(options = {}){
 	/**
 	 * File name for the WSDL corresponding to WSAA
 	 *
@@ -69,208 +85,173 @@ function Afip(options){
 	 **/
 	this.CUIT;
 
-	/**
-	 * Implemented Web Services
-	 *
-	 * @var array[string]
-	 **/
-	this.implemented_ws = [
-		'ElectronicBilling',
-		'RegisterScopeFour',
-		'RegisterScopeFive',
-		'RegisterScopeTen'
-	];
-
+	// Create an Afip instance if it is not
 	if (!(this instanceof Afip)) {return new Afip(options)}
 
-	options = options || {};	
 
 	if (!options.hasOwnProperty('CUIT')) {throw new Error("CUIT field is required in options array");}
-	else{this.CUIT = options['CUIT'];}
+	
+	// Define default options
 	if (!options.hasOwnProperty('production')) {options['production'] = false;}
 	if (!options.hasOwnProperty('passphrase')) {options['passphrase'] = 'xxxxx';}
 	if (!options.hasOwnProperty('cert')) {options['cert'] = 'cert';}
 	if (!options.hasOwnProperty('key')) {options['key'] = 'key';}
+	if (!options.hasOwnProperty('res_folder')) {options['res_folder'] = __dirname+'/Afip_res/';}
+	if (!options.hasOwnProperty('ta_folder')) {options['ta_folder'] = __dirname+'/Afip_res/';}
+	if (!options['production'] !== true) {options['production'] = false;}
 
 	this.options = options;
 
-
-	this.RES_FOLDER = __dirname+'/Afip_res/';
+	this.RES_FOLDER = options['res_folder'];
+	this.TA_FOLDER 	= options['ta_folder'];
 	this.CERT 		= path.resolve(this.RES_FOLDER, options['cert']);
 	this.PRIVATEKEY = path.resolve(this.RES_FOLDER, options['key']);
 	this.WSAA_WSDL 	= this.RES_FOLDER+'wsaa.wsdl';
 
-	if (options['production'] === true) {
+	if (options['production']) {
 		this.WSAA_URL = 'https://wsaa.afip.gov.ar/ws/services/LoginCms';
 	}
-	else{
+	else {
 		this.WSAA_URL = 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms';
 	}
 
-	var self = this;
-
-	for (var i = 0; i < this.implemented_ws.length; i++) {
-		defile_ws_getter(this.implemented_ws[i]);
-	}
-
-	function defile_ws_getter(ws) {
-		Object.defineProperty(self, ws, {
-			enumerable: true, 
-			configurable: false,
-			get : () => { 
-				if (self['__'+ws+'__']) {
-					return self['__'+ws+'__'];
-				}
-				else{
-					try{
-						var WSClass = require (__dirname+'/Class/'+ws+'.js');
-					}
-					catch(e){
-						throw new Error('File '+__dirname+'/Class/'+ws+'.js is required');
-					}
-
-					return self['__'+ws+'__'] = new WSClass(self);
-				}
-			}
-		});
-	}
+	//this.ElectronicBilling 		= new ElectronicBilling(this);
+	//this.RegisterScopeFour 		= new RegisterScopeFour(this);
+	//this.RegisterScopeFive 		= new RegisterScopeFive(this);
+	//this.RegisterScopeTen 		= new RegisterScopeTen(this);
+	//this.RegisterScopeThirteen 	= new RegisterScopeThirteen(this);
 }
 
 /**
  * Gets token authorization for an AFIP Web Service
  *
- * @since 0.1
- *
- * @param string service Service for token authorization
- * @param function(error,ta) callback
+ * @param service Service for token authorization
  **/
-Afip.prototype.GetServiceTA = function(service, callback, recreate)
-{
-	recreate = recreate === false ? false : true;
-	var ta_file = this.RES_FOLDER+'TA-'+this.options['CUIT']+'-'+service+'.json';
+Afip.prototype.GetServiceTA = async function(service, firstTry = true) {
+	// Declare token authorization file path
+	const taFilePath = path.resolve(
+		this.TA_FOLDER,
+		`TA-${this.options['CUIT']}-${service}${this.options['production'] ? '-production' : ''}.json`
+	);
 
-	fs.access(ta_file, fs.constants.F_OK, (err) => {
-		if (!err) {
-			var ta_data 		= require(ta_file),
-				actual_time 	= new Date(Date.now()+600000),
-				expiration_time = new Date(ta_data.header[1].expirationtime);
+	// Check if token authorization file exists
+	const taFileAccessError = await new Promise((resolve) => {
+		fs.access(taFilePath, fs.constants.F_OK, resolve);
+	}); 
 
-			if (actual_time < expiration_time) {
-				callback(null,{
-					token : ta_data.credentials.token,
-					sign : ta_data.credentials.sign
-				});
-				return;
-			}
-			else if (recreate === false){
-				callback('Error getting Token Autorization');
-				return;
+	// If have access to token authorization file
+	if (!taFileAccessError) {
+		const taData = require(taFilePath);
+		const actualTime = new Date(Date.now() + 600000);
+		const expirationTime = new Date(taData.header[1].expirationtime);
+
+		// Delete TA cache
+		delete require.cache[require.resolve(taFilePath)];
+
+		if (actualTime < expirationTime) {
+			// Return token authorization
+			return {
+				token : taData.credentials.token,
+				sign : taData.credentials.sign
 			}
 		}
+		// Throw error if this is not the first try to get token authorization
+		else if (firstTry === false){
+			throw new Error('Error getting Token Autorization');
+		}
+	}
 
-		this.CreateServiceTA(service)
-		.then(()=>{this.GetServiceTA(service, callback, false);})
-		.catch((err)=>{callback('Error getting Token Autorization '+err)})
+	// Create token authorization file
+	await this.CreateServiceTA(service).catch(err => {
+		throw new Error(`Error getting Token Autorization ${err}`)
 	});
+
+	// Try to get token authorization one more time
+	return await this.GetServiceTA(service, false);
 }
 
 /**
  * Create an TA from WSAA
  *
- * Request to WSAA for a tokent authorization for service and save this
- * in a xml file
+ * Request to WSAA for a tokent authorization for service 
+ * and save this in a json file
  *
- * @since 0.1
- *
- * @param string service 	Service for token authorization
- *
- * @return Promise 
+ * @param service Service for token authorization
  **/
-Afip.prototype.CreateServiceTA = function(service)
-{
-	return new Promise((resolve,reject)=>{
-		// Create TRA
-		var date 	= new Date(),
-			tra 	= '<?xml version="1.0" encoding="UTF-8" ?><loginTicketRequest version="1.0"><header><uniqueId>{uniqueId}</uniqueId><generationTime>{generationTime}</generationTime><expirationTime>{expirationTime}</expirationTime></header><service>{service}</service></loginTicketRequest>';
+Afip.prototype.CreateServiceTA = async function(service) {
+	const date = new Date();
+	
+	// Tokent request authorization XML
+	const tra = (`<?xml version="1.0" encoding="UTF-8" ?>
+	<loginTicketRequest version="1.0">
+		<header>
+			<uniqueId>${Math.floor(date.getTime() / 1000)}</uniqueId>
+			<generationTime>${new Date(date.getTime() - 600000).toISOString()}</generationTime>
+			<expirationTime>${new Date(date.getTime() + 600000).toISOString()}</expirationTime>
+		</header>
+		<service>${service}</service>
+	</loginTicketRequest>`).trim();
 
-		tra = tra.replace('{uniqueId}', Math.floor(date.getTime()/1000));
-		tra = tra.replace('{generationTime}', new Date(date.getTime()-600000).toISOString());
-		tra = tra.replace('{expirationTime}', new Date(date.getTime()+600000).toISOString());
-		tra = tra.replace('{service}', service);
-		tra = tra.trim();
-		
-		// Get cert
-		const certPromise = new Promise((resolve, reject) => {
-			fs.readFile(this.CERT, { encoding:'utf8' }, (err, data) => err ? reject(err) : resolve(data));
-		});
-			
-		// Get key
-		const keyPromise = new Promise((resolve, reject) => {
-			fs.readFile(this.PRIVATEKEY, { encoding:'utf8' }, (err, data) => err ? reject(err) : resolve(data));
-		});
-
-		// Sign TRA
-		Promise.all([certPromise, keyPromise]).then(([cert, key]) => {
-			const p7 = forge.pkcs7.createSignedData();
-			p7.content = forge.util.createBuffer(tra, "utf8");
-			p7.addCertificate(cert);
-			p7.addSigner({
-				authenticatedAttributes: [{
-					type: forge.pki.oids.contentType,
-					value: forge.pki.oids.data,
-				}, 
-				{
-					type: forge.pki.oids.messageDigest
-				}, 
-				{
-					type: forge.pki.oids.signingTime, 
-					value: new Date()
-				}],
-				certificate: cert,
-				digestAlgorithm: forge.pki.oids.sha256,
-				key: key,
-			});
-			
-			p7.sign();
-
-			const bytes = forge.asn1.toDer(p7.toAsn1()).getBytes();
-			
-			return Buffer.from(bytes, "binary").toString("base64");
-		})
-		.then((result)=> {
-			var args = {in0: result},
-				options = {disableCache:true, returnFault:true};
-
-			// Create SOAP client
-			soap.createClient(this.WSAA_WSDL, options,(err, client)=> {
-				if (err) {reject(err);return;}
-
-				// Request Token
-				client.loginCms(args,(err, result)=> {
-					if (err) {reject(err);return;}
-
-					var parseString = require('xml2js').parseString;
-
-					parseString(result.loginCmsReturn, {
-						normalizeTags: true,
-						normalize: true,
-						explicitArray: false,
-						attrkey: 'header',
-						tagNameProcessors: [(key) => { return key.replace('soapenv:', ''); }]
-					}, (err, res) => {
-						if (err) {reject(err);return;}
-
-						var fs = require('fs');
-						// Save Token data in json file						
-						fs.writeFile(this.RES_FOLDER+'TA-'+this.options['CUIT']+'-'+service+'.json', JSON.stringify(res.loginticketresponse), (err) => {
-							if (err) {reject(err);return;}
-							resolve(true)
-						});
-					});
-				});
-			});
-
-		})
-		.catch(function (err) {reject(err)});
+	// Get cert file content
+	const certPromise = new Promise((resolve, reject) => {
+		fs.readFile(this.CERT, { encoding:'utf8' }, (err, data) => err ? reject(err) : resolve(data));
 	});
+		
+	// Get key file content
+	const keyPromise = new Promise((resolve, reject) => {
+		fs.readFile(this.PRIVATEKEY, { encoding:'utf8' }, (err, data) => err ? reject(err) : resolve(data));
+	});
+
+	// Wait for cert and key content
+	const [cert, key] = await Promise.all([certPromise, keyPromise]);
+
+	// Sign Tokent request authorization XML
+	const p7 = forge.pkcs7.createSignedData();
+	p7.content = forge.util.createBuffer(tra, "utf8");
+	p7.addCertificate(cert);
+	p7.addSigner({
+		authenticatedAttributes: [{
+			type: forge.pki.oids.contentType,
+			value: forge.pki.oids.data,
+		}, 
+		{
+			type: forge.pki.oids.messageDigest
+		}, 
+		{
+			type: forge.pki.oids.signingTime, 
+			value: new Date()
+		}],
+		certificate: cert,
+		digestAlgorithm: forge.pki.oids.sha256,
+		key: key,
+	});
+	p7.sign();
+	const bytes = forge.asn1.toDer(p7.toAsn1()).getBytes();
+	const signedTRA = Buffer.from(bytes, "binary").toString("base64");
+
+	// SOAP Client options
+	const soapClientOptions = { disableCache:true, returnFault:true };
+
+	// Create SOAP client
+	const soapClient = await soap.createClientAsync(this.WSAA_WSDL, soapClientOptions);
+
+	// Arguments for soap client request 
+	const loginArguments = { in0: signedTRA };
+	
+	// Call loginCms SOAP method
+	const loginCmsResult = await soapClient.loginCmsAsync(loginArguments)
+
+	// Parse loginCmsReturn to JSON 
+	const res = await xmlParser.parseStringPromise(loginCmsResult.loginCmsReturn); 
+
+	// Token authorization file name
+	const taFileName = `TA-${this.options['CUIT']}-${service}${this.options['production'] ? '-production' : ''}.json`;
+
+	// Save Token authorization data to json file
+	await (new Promise((resolve, reject) => {
+		fs.writeFile(taFileName, JSON.stringify(res.loginticketresponse), (err) => {
+			if (err) {reject(err);return;}
+			resolve();
+		});
+	}));
 }

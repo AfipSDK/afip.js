d7971284f5af54b8a37c08d50e5ad020b6746b29
Ivan Muñoz <ivanalemunioz@gmail.com>
2019-09-12 16:49:59 -0300

    Register scope four and ten web services implemented

--- src/Afip.js
+++ src/Afip.js
@@ -15,9 +15,9 @@ var xmlParser = new xml2js.Parser({
 
 // Available Web Services
 //const ElectronicBilling = require('./Class/ElectronicBilling');
-//const RegisterScopeFour = require('./Class/RegisterScopeFour');
+const RegisterScopeFour = require('./Class/RegisterScopeFour');
 const RegisterScopeFive = require('./Class/RegisterScopeFive');
-//const RegisterScopeTen = require('./Class/RegisterScopeTen');
+const RegisterScopeTen = require('./Class/RegisterScopeTen');
 const RegisterScopeThirteen = require('./Class/RegisterScopeThirteen');
 
 /**
@@ -117,9 +117,9 @@ function Afip(options = {}){
 	}
 
 	//this.ElectronicBilling 		= new ElectronicBilling(this);
-	//this.RegisterScopeFour 		= new RegisterScopeFour(this);
+	this.RegisterScopeFour 		= new RegisterScopeFour(this);
 	this.RegisterScopeFive 		= new RegisterScopeFive(this);
-	//this.RegisterScopeTen 		= new RegisterScopeTen(this);
+	this.RegisterScopeTen 		= new RegisterScopeTen(this);
 	this.RegisterScopeThirteen 	= new RegisterScopeThirteen(this);
 }
 
---
+++ src/Class/RegisterScopeTen.js
@@ -0,0 +1,73 @@
+const AfipWebService = require('./AfipWebService');
+
+/**
+ * SDK for AFIP Register Scope Ten (ws_sr_padron_a10)
+ * 
+ * @link http://www.afip.gob.ar/ws/ws_sr_padron_a10/manual_ws_sr_padron_a10_v1.1. WS Specification
+ **/
+module.exports = class RegisterScopeTen extends AfipWebService {
+	constructor(afip){
+		const options = {
+			soapV12: false,
+			WSDL: 'ws_sr_padron_a10-production.wsdl',
+			URL: 'https://aws.afip.gov.ar/sr-padron/webservices/personaServiceA10',
+			WSDL_TEST: 'ws_sr_padron_a10.wsdl',
+			URL_TEST: 'https://awshomo.afip.gov.ar/sr-padron/webservices/personaServiceA10',
+			afip
+		}
+
+		super(options);
+	}
+	/**
+	 * Asks to web service for servers status {@see WS 
+	 * Specification item 3.1}
+	 *
+	 * @return object { appserver : Web Service status, 
+	 * dbserver : Database status, authserver : Autentication 
+	 * server status}
+	 **/
+	async getServerStatus() {
+		return this.executeRequest('dummy');
+	}
+
+	/**
+	 * Asks to web service for taxpayer details {@see WS 
+	 * Specification item 3.2}
+	 *
+	 * @throws Exception if exists an error in response 
+	 *
+	 * @return object|null if taxpayer does not exists, return null,  
+	 * if it exists, returns full response {@see 
+	 * WS Specification item 3.2.2}
+	 **/
+	async getTaxpayerDetails(identifier) {
+		// Get token and sign
+		let { token, sign } = await this.afip.GetServiceTA('ws_sr_padron_a10');
+
+		// Prepare SOAP params
+		let params = {
+			token, sign,
+			cuitRepresentada: this.afip.CUIT,
+			idPersona: identifier
+		};
+		
+		return this.executeRequest('getPersona', params)
+		.catch(err => { if (err.message.indexOf('No existe') !== -1) { return null } else { throw err }});
+	}
+
+	/**
+	 * Send request to AFIP servers
+	 * 
+	 * @param operation SOAP operation to execute 
+	 * @param params Parameters to send
+	 *
+	 * @return mixed Operation results 
+	 **/
+	async executeRequest(operation, params = {})
+	{
+		let results = await super.executeRequest(operation, params);
+
+		return results[operation === 'getPersona' ? 'personaReturn' : 'return'];
+	}
+}
+

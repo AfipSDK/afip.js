b43b879ada18ba652ffc4bca4ae62dca1cb03be6
Ivan Muñoz <ivanalemunioz@gmail.com>
2023-09-17 20:57:32 -0300

    Update sdk version

--- src/Afip.js
+++ src/Afip.js
@@ -45,7 +45,7 @@ function Afip(options = {}){
 	/**
 	 * SDK version
 	 **/
-	this.sdk_version_number = '0.7.8';
+	this.sdk_version_number = '0.8.0';
 
 	/**
 	 * File name for the WSDL corresponding to WSAA
